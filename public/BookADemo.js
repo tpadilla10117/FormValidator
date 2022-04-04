af6da490546ffb42084708fe7305f7d9a6db2e0d
Trinidad Padilla <tpadilla101@live.com>
2022-04-04 14:25:49 -0700

    need to work on page redirects

--- public/BookADemo.js
+++ public/BookADemo.js
@@ -1,3 +1,65 @@
+{/* <form class="bookADemo_form" onsubmit={}>
 
+        <div class="image_parent_container">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="calendardays_img" height="64" width="49"><path d="M160 32V64H288V32C288 14.33 302.3 0 320 0C337.7 0 352 14.33 352 32V64H400C426.5 64 448 85.49 448 112V160H0V112C0 85.49 21.49 64 48 64H96V32C96 14.33 110.3 0 128 0C145.7 0 160 14.33 160 32zM0 192H448V464C448 490.5 426.5 512 400 512H48C21.49 512 0 490.5 0 464V192zM64 304C64 312.8 71.16 320 80 320H112C120.8 320 128 312.8 128 304V272C128 263.2 120.8 256 112 256H80C71.16 256 64 263.2 64 272V304zM192 304C192 312.8 199.2 320 208 320H240C248.8 320 256 312.8 256 304V272C256 263.2 248.8 256 240 256H208C199.2 256 192 263.2 192 272V304zM336 256C327.2 256 320 263.2 320 272V304C320 312.8 327.2 320 336 320H368C376.8 320 384 312.8 384 304V272C384 263.2 376.8 256 368 256H336zM64 432C64 440.8 71.16 448 80 448H112C120.8 448 128 440.8 128 432V400C128 391.2 120.8 384 112 384H80C71.16 384 64 391.2 64 400V432zM208 384C199.2 384 192 391.2 192 400V432C192 440.8 199.2 448 208 448H240C248.8 448 256 440.8 256 432V400C256 391.2 248.8 384 240 384H208zM320 432C320 440.8 327.2 448 336 448H368C376.8 448 384 440.8 384 432V400C384 391.2 376.8 384 368 384H336C327.2 384 320 391.2 320 400V432z" fill="url(#grad)"/>
+          <defs>
+            <linearGradient id="grad" x1="0%" y1="30%" x2="0%" y2="100%">
+              <stop offset="0%" stop-color="#AA00FF" stop-opacity="1" />
+              <stop offset="80%" stop-color="#00A3D7" stop-opacity="1" />
 
+            </linearGradient>
+          </defs>
+          
+          </svg>
+        </div>
 
+        <div class="bookADemo_heading_parent">
+          <h1>Book a Demo</h1>
+        </div>
+
+        <div class="bookADemo_subheading_parent">
+          <h2>Help us customize your demo by answering the following questions below.</h2>
+        </div>
+
+        <div class="bookADemo_inputBlock">
+
+          <h3 class="bookADemo_subheader">Business Email</h3>
+          
+            <input class="bookADemo_form_input" placeholder="Enter your business email">
+            
+            <div class="bookADemo_dropdown_parentWrapper">
+
+              <h3 class="bookADemo_subheader">Business Size</h3>
+              <!-- TODO: Need to grab input data -->
+              <div class="bookADemo_form_dropbtn" onclick={dropdownToggler()}>
+                  <p>{dropdownArr}</p>
+    
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class='btnicon' height='14' width='14' fill="#777582" ><path d="M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z"/></svg>
+                
+              </div>
+
+              <div id="bookADemo_dropdown_element" class="dropdown-content">
+                <ul id="bookADemo_dropdown"></ul>
+              </div>
+
+            </div>
+        </div>
+          
+        <div class="bookADemo_radioBlock">
+          
+          <div>
+            <h3 class="bookADemo_subheader">What is the most important thing to you in a solution?</h3>
+          </div>
+
+          <!-- TODO: Radio Buttons -->
+          <div id="radiobtn-container">
+            <ul id="radiobtn-parentwrapper"></ul>
+          </div>
+
+          <div class="bookADemo_btn_container">
+            <button id="bookADemo_form_btn" class="bookADemo_form_btn" onclick={btnClicked()}>Continue to Schedule a Time</button>
+          </div>
+
+        </div>
+            
+      </form> */}
